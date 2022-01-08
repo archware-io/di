ee74c9479ac132960ab9a109c152be038ab0a780
Marek Dulowski <dulowski.marek@gmail.com>
2022-01-09 00:09:28 +0100

    Add resolving logic

--- src/Injector.ts
+++ src/Injector.ts
@@ -2,10 +2,24 @@ import type { Registrable } from './Registrable';
 import { asClass } from './asClass';
 
 export class Injector {
-  constructor(private registry = new Map<any, Registrable<any>>()) {}
+  constructor(private registry = new Map<any, Registrable<any>>(), private container = new Map<any, any>()) {}
 
   public resolve<T>(target: abstract new (...args: any[]) => T): T;
   public resolve<T>(target: any): T {
-    const registrable = this.registry.get(target) ?? asClass(target);
+    if (this.container.has(target)) {
+      return this.container.get(target);
+    }
+
+    const {
+      descriptor: { factory, dependencies, isSingleton },
+    } = this.registry.get(target) ?? asClass(target);
+
+    const instance = factory(...dependencies.map((dependency) => this.resolve(dependency)));
+
+    if (isSingleton) {
+      this.container.set(target, instance);
+    }
+
+    return instance;
   }
 }
