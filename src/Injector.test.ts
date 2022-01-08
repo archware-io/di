85ef84760b7a90f88870cf2abca47f6f025807c6
Marek Dulowski <dulowski.marek@gmail.com>
2022-01-09 00:57:13 +0100

    Implement baseline behaviour tests

--- src/Injector.test.ts
+++ src/Injector.test.ts
@@ -1,11 +1,108 @@
 import { Injector } from './Injector';
+import { Resolvable } from './Resolvable';
+import { asValue } from './asValue';
+import { asImplementation } from './asImplementation';
 
 describe("Injector", () => {
-  const injector = new Injector();
-
   test("resolve a class", () => {
+    // given
+    const injector = new Injector();
+
+    @Resolvable()
     class Target { }
 
-    expect(() => injector.resolve(Target)).toThrowError();
+    // then
+    expect(injector.resolve(Target)).toBeInstanceOf(Target);
+  });
+
+  test("fail resolving class when not decorated", () => {
+    // given
+    const injector = new Injector();
+
+    class Target { }
+
+    // then
+    expect(() => injector.resolve(Target)).toThrow();
+  });
+
+  test("resolve a value", () => {
+    // given
+    const injector = new Injector();
+
+    class Target { }
+
+    // when
+    injector.register(asValue(Target, new Target()));
+
+    // then
+    expect(injector.resolve(Target)).toBeInstanceOf(Target);
+  });
+
+  test("resolve an implementation", () => {
+    // given
+    const injector = new Injector();
+
+    interface Pet {
+      speak(): string;
+    }
+
+    abstract class Pet { }
+
+    @Resolvable()
+    class Dog implements Pet {
+      speak(): string {
+        return 'woof';
+      }
+    }
+
+    // when
+    injector.register(asImplementation(Pet, Dog));
+
+    // then
+    expect(injector.resolve(Pet)).toBeInstanceOf(Dog);
+  });
+
+  describe("singletons", () => {
+    test("if not declared singleton, return new instances", () => {
+      // given
+      const injector = new Injector();
+      const instanceCreated = jest.fn();
+
+      @Resolvable()
+      class NotSingleton {
+        constructor() {
+          instanceCreated();
+        }
+      }
+
+      // when
+      injector.resolve(NotSingleton);
+      injector.resolve(NotSingleton);
+
+      // then
+      expect(instanceCreated).toHaveBeenCalledTimes(2);
+    });
+
+    test("if declared singleton, return the same instance", () => {
+      // given
+      const injector = new Injector();
+      const instanceCreated = jest.fn();
+
+      @Resolvable({
+        singleton: true,
+      })
+      class Singleton {
+        constructor() {
+          instanceCreated();
+        }
+      }
+
+      // when
+      injector.resolve(Singleton);
+      injector.resolve(Singleton);
+
+      // then
+      expect(instanceCreated).toHaveBeenCalledTimes(2);
+    });
   });
 });
