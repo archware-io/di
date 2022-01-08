dbdf0fe3981420f75b6c36bc5e46fe7c5da826b0
Marek Dulowski <dulowski.marek@gmail.com>
2022-01-08 23:34:33 +0100

    Implement Resolvable decorator

---
+++ src/Resolvable.ts
@@ -0,0 +1,18 @@
+export const Resolvable = (): ClassDecorator => (target: any) => {
+  setDecorated(target);
+};
+
+const IS_DECORATED = Symbol('IS_DECORATED');
+
+export const setDecorated = (target: any) => {
+  Object.defineProperty(target, IS_DECORATED, {
+    value: true,
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  });
+};
+
+export const isDecorated = (target: any): boolean => {
+  return target[IS_DECORATED] === true;
+};
